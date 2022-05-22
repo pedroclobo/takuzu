21d7f37b1c0c3faab66042841a9c27bf8147bc98
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-05-22 17:19:30 +0100

    Naive implementation

--- src/takuzu.py
+++ src/takuzu.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 41:
+# 99115 Pedro Lobo
+# 99079 Guilherme Pascoal
 
 from sys import stdin
 import numpy as np
@@ -23,12 +23,15 @@ class TakuzuState:
 	state_id = 0
 
 	def __init__(self, board):
-		self.board = board.copy()
+		self.board = board
 		self.id = TakuzuState.state_id
 		TakuzuState.state_id += 1
 
 	def __lt__(self, other):
 		return self.id < other.id
+
+	def get_board(self):
+		return self.board.copy()
 
 	# TODO: outros metodos da classe
 
@@ -46,35 +49,17 @@ class Board:
 
 	def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
 		"""Devolve os valores imediatamente abaixo e acima, respectivamente."""
-		numbers = ()
+		first = self.board[row - 1][col] if row != 0 else None
+		sec = self.board[row + 1][col] if row != self.dimension - 1 else None
 
-		if row != 0:
-			numbers += (self.board[row - 1][col], )
-		else:
-			numbers += (None, )
-
-		if row != self.dimension - 1:
-			numbers += (self.board[row + 1][col], )
-		else:
-			numbers += (None, )
-
-		return numbers
+		return (first, sec)
 
 	def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
 		"""Devolve os valores imediatamente à esquerda e à direita, respectivamente."""
-		numbers = ()
+		first = self.board[row][col - 1] if col != 0 else None
+		sec = self.board[row][col + 1] if col != self.dimension - 1 else None
 
-		if col != 0:
-			numbers += (self.board[row][col - 1], )
-		else:
-			numbers += (None, )
-
-		if col != self.dimension - 1:
-			numbers += (self.board[row][col + 1], )
-		else:
-			numbers += (None, )
-
-		return numbers
+		return (first, sec)
 
 	@staticmethod
 	def parse_instance_from_stdin():
@@ -90,13 +75,94 @@ class Board:
 		dimension = int(input())
 		board = []
 
-		for i in range(dimension):
+		while dimension > 0:
 			board += [[int(x) for x in stdin.readline().strip().split("\t")]]
+			dimension -= 1
 
 		return Board(board)
 
 	def copy(self):
-		return np.copy(self.board)
+		return Board(self.board)
+
+	def place_number(self, row: int, col: int, number: int):
+		self.board[row][col] = number
+
+	def is_free_position(self, row: int, col: int):
+		return self.get_number(row, col) == 2
+
+	def get_free_positions(self):
+		positions = ()
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				if self.is_free_position(i, j):
+					positions += ((i, j), )
+
+		return positions
+
+	def check_numbers(self):
+		# Line check
+		for i in range(self.dimension):
+			zeros = np.count_nonzero(self.board[i, ] == 0)
+			ones = np.count_nonzero(self.board[i, ] == 1)
+
+			if self.dimension % 2 == 0:
+				if zeros != ones:
+					return False
+			else:
+				if zeros > ones + 1 or ones > zeros + 1:
+					return False
+
+
+        # Column check
+		for j in range(self.dimension):
+			zeros = np.count_nonzero(self.board[:, j] == 0)
+			ones = np.count_nonzero(self.board[:, j] == 1)
+
+			if self.dimension % 2 == 0:
+				if zeros != ones:
+					return False
+			else:
+				if zeros > ones + 1 or ones > zeros + 1:
+					return False
+
+		return True
+
+	def check_adjacency(self):
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				number = self.get_number(i, j)
+				adjacent_h = self.adjacent_horizontal_numbers(i, j)
+				adjacent_v = self.adjacent_vertical_numbers(i, j)
+
+				if (adjacent_h + (number, )).count(number) == 3 or \
+				   (adjacent_v + (number, )).count(number) == 3:
+					return False
+
+		return True
+
+	def check_vector_difference(self):
+		return not (self.board == self.board[0]).all() and \
+		       not (self.board.T == self.board.T[0]).all()
+
+	def check_finished_board(self):
+		return self.check_numbers() and \
+		       self.check_adjacency() and \
+		       self.check_vector_difference() and \
+		       2 not in self.board
+
+	def __str__(self):
+		string = ""
+
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				string += str(self.get_number(i, j))
+				if j != self.dimension - 1:
+					string += "\t"
+
+			if i != self.dimension - 1:
+				string += "\n"
+
+		return string
 
 	# TODO: outros metodos da classe
 
@@ -105,28 +171,36 @@ class Takuzu(Problem):
 
 	def __init__(self, board: Board):
 		"""O construtor especifica o estado inicial."""
+		super().__init__(TakuzuState(board))
 		self.state = TakuzuState(board)
 
 	def actions(self, state: TakuzuState):
 		"""Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
-		# TODO
-		pass
+		actions = ()
+
+		for (row, col) in state.get_board().get_free_positions():
+			for number in (0, 1):
+				actions += ((row, col, number), )
+
+		return actions
 
 	def result(self, state: TakuzuState, action):
 		"""Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-		# TODO
-		pass
+		(row, col, number) = action
+		board = state.get_board()
+		board.place_number(row, col, number)
+
+		return TakuzuState(board)
 
 	def goal_test(self, state: TakuzuState):
 		"""Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
-		# TODO
-		pass
+		return state.get_board().check_finished_board()
 
 	def h(self, node: Node):
 		"""Função heuristica utilizada para a procura A*."""
@@ -137,9 +211,16 @@ class Takuzu(Problem):
 
 
 if __name__ == "__main__":
-	# TODO:
-	# Ler o ficheiro do standard input,
-	# Usar uma técnica de procura para resolver a instância,
-	# Retirar a solução a partir do nó resultante,
-	# Imprimir para o standard output no formato indicado.
-	pass
+	board = Board.parse_instance_from_stdin()
+
+	#node = breadth_first_tree_search(Takuzu(board))
+	node = depth_first_tree_search(Takuzu(board.copy()))
+	#node = greedy_search(Takuzu(board))
+	#node = astar_search(Takuzu(board))
+
+	solution = node.solution()
+
+	for (row, col, number) in solution:
+		board.place_number(row, col, number)
+
+	print(board)
