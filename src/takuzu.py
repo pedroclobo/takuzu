eaf8f6bddc2e905e3ab0b71057a48f2bd6e2c305
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-05-29 11:18:16 +0100

    Implement board __eq__ for better graph search

--- src/takuzu.py
+++ src/takuzu.py
@@ -1,17 +1,21 @@
 # Grupo 41:
-# 99115 Pedro Lobo
 # 99079 Guilherme Pascoal
+# 99115 Pedro Lobo
 
 from sys import stdin
 import numpy as np
 from search import (
     Problem,
     Node,
+    compare_searchers,
     astar_search,
+    breadth_first_graph_search,
     breadth_first_tree_search,
+    depth_first_graph_search,
     depth_first_tree_search,
     greedy_search,
-    recursive_best_first_search,
+    iterative_deepening_search,
+    uniform_cost_search,
 )
 
 
@@ -24,7 +28,14 @@ class TakuzuState:
 		TakuzuState.state_id += 1
 
 	def __lt__(self, other):
-		return len(self.board.get_free_positions()) < len(other.board.get_free_positions())
+		return len(self.board.get_free_positions()) < \
+		       len(other.board.get_free_positions())
+
+	def __eq__(self, other):
+		return self.board == other.board
+
+	def __hash__(self):
+		return self.id
 
 	def get_board(self):
 		return self.board.copy()
@@ -39,21 +50,28 @@ class Board:
 		self.board = np.array(board)
 		self.dimension = len(self.board)
 
+		# Compute free positions
+		self.free = []
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				if self.get_number(i, j) == 2:
+					self.free.append((i, j))
+
 	def get_number(self, row: int, col: int) -> int:
 		"""Return value in given position."""
 		return self.board[row, col]
 
 	def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
 		"""Return values under and above the given position."""
-		first = self.board[row + 1][col] if row != self.dimension - 1 else None
-		sec = self.board[row - 1][col] if row != 0 else None
+		first = self.get_number(row + 1, col) if row != self.dimension - 1 else None
+		sec = self.get_number(row - 1, col) if row != 0 else None
 
 		return (first, sec)
 
 	def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
 		"""Return values to left and to the right of the given position."""
-		first = self.board[row][col - 1] if col != 0 else None
-		sec = self.board[row][col + 1] if col != self.dimension - 1 else None
+		first = self.get_number(row, col - 1) if col != 0 else None
+		sec = self.get_number(row, col + 1) if col != self.dimension - 1 else None
 
 		return (first, sec)
 
@@ -76,16 +94,11 @@ class Board:
 	def place_number(self, row: int, col: int, number: int):
 		"""Place number on board instance."""
 		self.board[row][col] = number
+		self.free = [el for el in self.free if el != (row, col)]
 
 	def get_free_positions(self):
 		"""Return coordinates of free position."""
-		positions = ()
-		for i in range(self.dimension):
-			for j in range(self.dimension):
-				if self.get_number(i, j) == 2:
-					positions += ((i, j), )
-
-		return positions
+		return self.free
 
 	def check_count_criteria(self):
 		"""Check if specified row or column has an equal amount of 0 and 1,
@@ -95,8 +108,8 @@ class Board:
 			ones = np.count_nonzero(self.board[i, ] == 1)
 
 			if (self.dimension % 2 == 0 and zeros != ones) or \
-               (self.dimension % 2 != 0 and \
-                   (zeros != ones + 1 or zeros + 1 != ones)):
+			   (self.dimension % 2 != 0 and \
+			   (zeros != ones + 1 or zeros + 1 != ones)):
 				return False
 
 		for j in range(self.dimension):
@@ -104,8 +117,8 @@ class Board:
 			ones = np.count_nonzero(self.board[:, j] == 1)
 
 			if (self.dimension % 2 == 0 and zeros != ones) or \
-               (self.dimension % 2 != 0 and \
-                   (zeros != ones + 1 or zeros + 1 != ones)):
+			   (self.dimension % 2 != 0 and \
+			   (zeros != ones + 1 or zeros + 1 != ones)):
 				return False
 
 		return True
@@ -119,7 +132,7 @@ class Board:
 				adjacent_v = self.adjacent_vertical_numbers(i, j)
 
 				if (adjacent_h + (number, )).count(number) == 3 or \
-                   (adjacent_v + (number, )).count(number) == 3:
+				   (adjacent_v + (number, )).count(number) == 3:
 					return False
 
 		return True
@@ -127,14 +140,17 @@ class Board:
 	def check_vector_inequality_criteria(self):
 		"""Check if all rows are different and if all columns are different."""
 		return not (self.board == self.board[0]).all() and \
-               not (self.board.T == self.board.T[0]).all()
+		       not (self.board.T == self.board.T[0]).all()
 
 	def check_resolved_board(self):
 		"""Check if board is resolved."""
-		return self.check_count_criteria() and \
-               self.check_adjacency_criteria() and \
-               self.check_vector_inequality_criteria() and \
-               2 not in self.board
+		return self.is_full() and \
+		       self.check_count_criteria() and \
+		       self.check_adjacency_criteria() and \
+		       self.check_vector_inequality_criteria()
+
+	def __eq__(self, other):
+		return (self.board == other.board).all()
 
 	def __str__(self):
 		"""Board representation."""
@@ -164,24 +180,28 @@ class Board:
 
 		if self.dimension % 2 == 0:
 			if zeros_r > self.dimension / 2 or ones_r > self.dimension / 2 or \
-               zeros_c > self.dimension / 2 or ones_c > self.dimension / 2:
+			   zeros_c > self.dimension / 2 or ones_c > self.dimension / 2:
 				return False
 
 		else:
 			if zeros_r > self.dimension // 2 + 1 or ones_r > self.dimension // 2 + 1 or \
-               zeros_c > self.dimension // 2 + 1 or ones_c > self.dimension // 2 + 1:
+			   zeros_c > self.dimension // 2 + 1 or ones_c > self.dimension // 2 + 1:
 				return False
 
 		# Check adjacency criteria
 		if self.adjacent_vertical_numbers(row, col).count(number) > 2 or \
-           self.adjacent_horizontal_numbers(row, col).count(number) > 2:
+		   self.adjacent_horizontal_numbers(row, col).count(number) > 2:
 			return False
 
 		# Check for different rows and columns
 		return self.check_vector_inequality_criteria()
 
+	def is_full(self):
+		return len(self.free) == 0
+
 	def restrictions(self, row, col):
-		return len([el for el in self.adjacent_vertical_numbers(row, col) if el != None and el != 2]) + len([el for el in self.adjacent_horizontal_numbers(row, col) if el != None and el != 2])
+		return len([el for el in self.adjacent_vertical_numbers(row, col) if el != None and el != 2]) + \
+		       len([el for el in self.adjacent_horizontal_numbers(row, col) if el != None and el != 2])
 
 	# TODO: outros metodos da classe
 
@@ -219,11 +239,11 @@ class Takuzu(Problem):
 
 	def goal_test(self, state: TakuzuState):
 		"""Return True if 'state' if a goal state."""
-		return state.get_board().check_resolved_board()
+		return state.board.check_resolved_board()
 
 	def h(self, node: Node):
 		"""Heuristic for A*."""
-		board = node.state.get_board()
+		board = node.state.board
 
 		# Number of free positions
 		h1 = len(board.get_free_positions())
@@ -236,14 +256,26 @@ class Takuzu(Problem):
 if __name__ == "__main__":
 	board = Board.parse_instance_from_stdin()
 
-	#node = breadth_first_tree_search(Takuzu(board.copy()))
-	#node = depth_first_tree_search(Takuzu(board.copy()))
-	#node = greedy_search(Takuzu(board.copy()))
-	node = astar_search(Takuzu(board.copy()))
+	searchers = [
+	    astar_search,
+	    breadth_first_graph_search,
+	    breadth_first_tree_search,
+	    depth_first_graph_search,
+	    depth_first_tree_search,
+	    greedy_search,
+	    iterative_deepening_search,
+	    uniform_cost_search
+	]
 
-	solution = node.solution()
+	compare_searchers([Takuzu(board)], "", searchers)
 
-	for (row, col, number) in solution:
-		board.place_number(row, col, number)
+	#node = astar_search(Takuzu(board))
+	#node = breadth_first_graph_search(Takuzu(board))
+	#node = breadth_first_tree_search(Takuzu(board))
+	#node = depth_first_graph_search(Takuzu(board))
+	#node = depth_first_tree_search(Takuzu(board))
+	#node = greedy_search(Takuzu(board))
+	#node = iterative_deepening_search(Takuzu(board))
+	#node = uniform_cost_search(Takuzu(board))
 
-	print(board)
+	#print(node.state.board)
