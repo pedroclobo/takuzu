916df5e682bc53a90813d15ca335da279c8cc188
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-26 15:53:56 +0100

    Generate graphs

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ tests::
 	@$(MAKE) $(MFLAGS) -C tests
 
 relatorio::
-	(cd docs && pdflatex relatorio.tex)
+	(Rscript benchmarks/graficos.r && cd docs && pdflatex relatorio.tex)
 
 clean::
 	(rm -rf src/__pycache__ && rm -f tests/*.diff && rm -f docs/relatorio.pdf && find . | egrep '*.aux|*.log|*.out' | grep -v ".git" | grep -v "tests" | xargs \rm -f)
---
+++ benchmarks/graficos.r
@@ -0,0 +1,34 @@
+library(tidyr)
+library(ggplot2)
+
+tempos <- function() {
+	read.csv("benchmarks/tempos.csv", row.names = NULL) |>
+		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Tempo") |>
+		ggplot(aes(x = Teste, y = Tempo, fill = Algoritmo)) +
+		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev) +
+		labs(x = "Teste", y = "Tempo [ms]")
+
+	ggsave("benchmarks/tempos.png", device="png", height = 4, width = 6.5, dpi = 600)
+}
+
+expandidos <- function() {
+	read.csv("benchmarks/expandidos.csv", row.names = NULL) |>
+		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Nós Expandidos") |>
+		ggplot(aes(x = Teste, y = `Nós Expandidos`, fill = Algoritmo)) +
+		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev)
+
+	ggsave("benchmarks/expandidos.png", device="png", height = 4, width = 6.5, dpi = 600)
+}
+
+gerados <- function() {
+	read.csv("benchmarks/gerados.csv", row.names = NULL) |>
+		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Nós Gerados") |>
+		ggplot(aes(x = Teste, y = `Nós Gerados`, fill = Algoritmo)) +
+		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev)
+
+	ggsave("benchmarks/gerados.png", device="png", height = 4, width = 6.5, dpi = 600)
+}
+
+tempos()
+expandidos()
+gerados()
