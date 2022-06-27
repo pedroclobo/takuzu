0e925f08324d645eabb94f5646dec0fded670205
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-27 20:27:08 +0100

    Finish report and filter tests

--- benchmarks/graficos.r
+++ benchmarks/graficos.r
@@ -1,8 +1,10 @@
 library(tidyr)
 library(ggplot2)
 
+testes <- c("test02", "test03", "test04", "test08", "test09", "test13")
+
 tempos <- function() {
-	read.csv("benchmarks/tempos.csv", row.names = NULL) |>
+	read.csv("benchmarks/tempos.csv", row.names = NULL) |> subset(Teste %in% testes) |>
 		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Tempo") |>
 		ggplot(aes(x = Teste, y = Tempo, fill = Algoritmo)) +
 		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev) +
@@ -12,7 +14,7 @@ tempos <- function() {
 }
 
 expandidos <- function() {
-	read.csv("benchmarks/expandidos.csv", row.names = NULL) |>
+	read.csv("benchmarks/expandidos.csv", row.names = NULL) |> subset(Teste %in% testes) |>
 		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Nós Expandidos") |>
 		ggplot(aes(x = Teste, y = `Nós Expandidos`, fill = Algoritmo)) +
 		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev)
@@ -21,7 +23,7 @@ expandidos <- function() {
 }
 
 gerados <- function() {
-	read.csv("benchmarks/gerados.csv", row.names = NULL) |>
+	read.csv("benchmarks/gerados.csv", row.names = NULL) |> subset(Teste %in% testes) |>
 		pivot_longer(2:5, names_to = "Algoritmo", values_to = "Nós Gerados") |>
 		ggplot(aes(x = Teste, y = `Nós Gerados`, fill = Algoritmo)) +
 		geom_col(position="dodge") + coord_flip() + scale_x_discrete(limits=rev)
